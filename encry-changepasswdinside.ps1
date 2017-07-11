403add3b109e60dbeab2732a4817b9eb814c7eed
aidden-laoch <aidden.laoch@gmail.com>
2017-07-11 15:33:27 -0400

    Added script to run against directories
    This allows you to run Out-EncryptScript.ps1 against a directory. Creates a file with the same name and appends '.txt' for referencing.

---
+++ encry-changepasswdinside.ps1
@@ -0,0 +1,12 @@
+﻿param([string]$d = 'd')
+
+Import-Module ScriptModification
+
+Get-Command -Module ScriptModification
+
+Get-ChildItem $d -Filter *ps1 | Foreach-Object{
+    echo $_.FullName
+    Out-EncryptedScript $_.FullName password salt
+    }
+
+ls -h $d
